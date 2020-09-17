43874ea447ed99127a6e9301d798f3a58cd493b2
bat <bjoern.barschtipan@gmail.com>
2020-09-17 19:26:13 +0200

    Create command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use std::path::Path;
 
 mod findbench;
@@ -5,8 +6,25 @@ mod findbench;
 use findbench::find_executable_benchmarks;
 
 fn main() -> Result<(), std::io::Error> {
-    let cwd = Path::new("/home/bat/workspace").to_path_buf();
-    let benchmark_paths = find_executable_benchmarks(cwd);
+    let matches = App::new("beast")
+        .version("0.1")
+        .about("(be)nchmark (a)nalysis and (s)ummary (t)ool")
+        .arg(Arg::from_usage(
+            "[rootdir], -d, --dir=[DIR] 'root directory to use for benchmark search'",
+        ))
+        .get_matches();
+
+    let root_dir = match matches.value_of("rootdir") {
+        Some(valid_val) => Path::new(valid_val).to_path_buf(),
+        _ => match std::env::current_dir() {
+            Ok(path_buf) => path_buf,
+            Err(err) => panic!("Can't retrieve current directory: {:?}", err),
+        },
+    };
+
+    println!("Root scan directory: {:?}", root_dir.as_os_str());
+
+    let benchmark_paths = find_executable_benchmarks(root_dir);
     println!("{:?}", benchmark_paths);
     Ok(())
 }
