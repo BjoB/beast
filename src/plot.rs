9e7336275f141cfe08cbd47bb2e50704d2c79acb
bat <bjoern.barschtipan@gmail.com>
2020-09-24 18:50:22 +0200

    Add plotly example (WIP)

---
+++ src/plot.rs
@@ -0,0 +1,10 @@
+use plotly::{Bar, Plot};
+
+pub fn plot() {
+    let animals = vec!["giraffes", "orangutans", "monkeys"];
+    let t = Bar::new(animals, vec![20, 14, 23]);
+    let mut plot = Plot::new();
+    plot.add_trace(t);
+    plot.show();
+    println!("{}", plot.to_inline_html(Some("basic_bar_chart")));
+}
